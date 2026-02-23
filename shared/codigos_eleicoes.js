13c3b66cdd79940b75938f1f927243f65f46f18d
moacirdavidag <moacirdavidag@gmail.com>
2026-02-23 19:14:50 -0300

    feat: Ajustes na aplicação em andamento

--- shared/codigos_eleicoes.js
+++ shared/codigos_eleicoes.js
@@ -1,14 +1,211 @@
 const CODIGOS_ELEICOES = {
-    2020: {
-        turnos: {
-            1: 426, // 1º Turno
-            2: 427 // 2º Turno
-        },
-        cargos: {
-            11: 'Prefeito',
-            13: 'Vereador'
-        }
-    }
-}
+  1996: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      11: "Prefeito",
+      13: "Vereador",
+    },
+  },
+  1998: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      1: "Presidente",
+      2: "Vice-Presidente",
+      3: "Governador",
+      4: "Vice-Governador",
+      5: "Senador",
+      6: "Deputado Federal",
+      7: "Deputado Estadual",
+      8: "Deputado Distrital",
+    },
+  },
+  2000: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      11: "Prefeito",
+      13: "Vereador",
+    },
+  },
+  2002: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      1: "Presidente",
+      2: "Vice-Presidente",
+      3: "Governador",
+      4: "Vice-Governador",
+      5: "Senador",
+      6: "Deputado Federal",
+      7: "Deputado Estadual",
+      8: "Deputado Distrital",
+    },
+  },
+  2004: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      11: "Prefeito",
+      13: "Vereador",
+    },
+  },
+  2006: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      1: "Presidente",
+      2: "Vice-Presidente",
+      3: "Governador",
+      4: "Vice-Governador",
+      5: "Senador",
+      6: "Deputado Federal",
+      7: "Deputado Estadual",
+      8: "Deputado Distrital",
+    },
+  },
+  2008: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      11: "Prefeito",
+      13: "Vereador",
+    },
+  },
+  2010: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      1: "Presidente",
+      2: "Vice-Presidente",
+      3: "Governador",
+      4: "Vice-Governador",
+      5: "Senador",
+      6: "Deputado Federal",
+      7: "Deputado Estadual",
+      8: "Deputado Distrital",
+    },
+  },
+  2012: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      11: "Prefeito",
+      13: "Vereador",
+    },
+  },
+  2014: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      1: "Presidente",
+      2: "Vice-Presidente",
+      3: "Governador",
+      4: "Vice-Governador",
+      5: "Senador",
+      6: "Deputado Federal",
+      7: "Deputado Estadual",
+      8: "Deputado Distrital",
+    },
+  },
+  2016: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      11: "Prefeito",
+      13: "Vereador",
+    },
+  },
+  2018: {
+    turnos: {
+      1: 297,
+    },
+    cargos: {
+      1: "Presidente",
+      2: "Vice-Presidente",
+      3: "Governador",
+      4: "Vice-Governador",
+      5: "Senador",
+      6: "Deputado Federal",
+      7: "Deputado Estadual",
+      8: "Deputado Distrital",
+    },
+  },
+  2020: {
+    turnos: {
+      1: 426,
+      2: 427,
+    },
+    cargos: {
+      11: "Prefeito",
+      13: "Vereador",
+    },
+  },
+  2022: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      1: "Presidente",
+      2: "Vice-Presidente",
+      3: "Governador",
+      4: "Vice-Governador",
+      5: "Senador",
+      6: "Deputado Federal",
+      7: "Deputado Estadual",
+      8: "Deputado Distrital",
+    },
+  },
+  2024: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      11: "Prefeito",
+      13: "Vereador",
+    },
+  },
+  2026: {
+    turnos: {
+      1: null,
+      2: null,
+    },
+    cargos: {
+      1: "Presidente",
+      2: "Vice-Presidente",
+      3: "Governador",
+      4: "Vice-Governador",
+      5: "Senador",
+      6: "Deputado Federal",
+      7: "Deputado Estadual",
+      8: "Deputado Distrital",
+    },
+  },
+};
 
 export default CODIGOS_ELEICOES;
